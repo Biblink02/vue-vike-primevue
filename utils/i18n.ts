eb0899e05cd1b8ec8f85a29313be24a44354b2ac
Biblink02 <alby.bot.24@gmail.com>
2026-03-06 02:18:55 +0100

    feat: add vue-i18n logic and default locales

---
+++ utils/i18n.ts
@@ -0,0 +1,13 @@
+import { createI18n } from "vue-i18n";
+import en from "./locales/en.json";
+import it from "./locales/it.json";
+
+export const i18n = createI18n({
+  legacy: false, // necessary for Composition API
+  locale: "en", // default locale
+  fallbackLocale: "en",
+  messages: {
+    en,
+    it,
+  },
+});
